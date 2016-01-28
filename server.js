0c9c1619a81600d1fb58887ff13c5b8281766738
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-01-28 17:44:55 -0500

    Adding in some diagnostics

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require("express");
+var express = require('express');
+var request = require('superagent');
 
 
 
@@ -7,9 +8,41 @@ var app = express();
 
 
 
-
+var diagnosticsURL = 'https://hooks.slack.com/services/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'
 
 var _ = require('lodash');
+
+
+
+
+
+
+var sendDiagmsg = function(text){
+	request
+		.post(diagnosticsURL)
+		.send({
+
+			"text": text,
+
+			"color": "#36a64f", // Can either be one of 'good', 'warning', 'danger', or any hex color code
+
+			// Fields are displayed in a table on the message
+			/*
+			"fields": [
+				{
+					"title": "Command Fired", // The title may not contain markup and will be escaped for you
+					"value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
+					"short": false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
+				}
+			]
+			*/
+		});
+}
+
+
+
+
+sendDiagmsg("Server restart!");
 
 
 
@@ -29,22 +62,31 @@ app.get('/roll', function(req, res){
 
 
 
+	sendDiagmsg("Roll Command Fired");
+
 
 
 
 	return res.status(200).send({
-		"response_type": "in_channel",
-		"text": num_dice+'d'+dice_type+': ' + _.sum(rolledDice),
-		"attachments": [
+		'response_type': 'in_channel',
+		'text': num_dice+'d'+dice_type+': ' + _.sum(rolledDice),
+		'attachments': [
 			{
-				"text": JSON.stringify(rolledDice)
+				'text': JSON.stringify(rolledDice)
 			}
 		]
 	});
 })
 
 
+var rollCmd = {
+	private : false, //sets it so your command will only be seen by
+	cmd : function(text, data){
 
+
+		//return a string for basic text, or object complex messaging
+	}
+}
 
 
 
