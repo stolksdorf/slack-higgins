6bf0c04f5e436229f084b5eb0eeac10247e64a04
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-01-28 17:04:10 -0500

    Basic roll command setup

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+
+
+
+var app = express();
+
+
+
+
+
+
+
+
+
+
+app.get('/roll', function(req, res){
+	console.log(req.params.text);
+
+
+	return res.status(200).send({
+		"text": "your message: " + req.params.text,
+		"attachments": [
+			{
+				"text":"Partly butty today and tomorrow"
+			}
+		]
+	});
+})
+
+})
+
+
+
+
+
+
+
+
+
+
+var port = process.env.PORT || 8000;
+
+app.listen(port);
