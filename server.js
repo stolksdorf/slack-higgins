1823192438f903537fc327ce23fa4716a26bea1a
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-01-28 17:12:35 -0500

    fixing

--- server.js
+++ server.js
@@ -14,14 +14,12 @@ var app = express();
 
 
 app.get('/roll', function(req, res){
-	console.log(req.params.text);
-
 
 	return res.status(200).send({
-		"text": "your message: " + req.params.text,
+		"text": "your message: " + req.query.text,
 		"attachments": [
 			{
-				"text":"Partly butty today and tomorrow"
+				"text": JSON.stringify(req.query, null, '  ')
 			}
 		]
 	});
