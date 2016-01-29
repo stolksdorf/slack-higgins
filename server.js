6a2ed584b53aff2d6dd785a02f47e4fab0bc23ec
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-01-28 19:06:05 -0500

    Adding in diagnostics messaging

--- server.js
+++ server.js
@@ -1,56 +1,14 @@
-var express = require('express');
-var request = require('superagent');
-
-
-
-var app = express();
-
-
-
-
-var diagnosticsURL = 'https://hooks.slack.com/services/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'
+require('app-module-path').addPath('./shared');
 
 var _ = require('lodash');
+var express = require('express');
+var app = express();
+
+var diag = require('diag');
 
 
 
-
-
-
-var sendDiagmsg = function(text, cb){
-	request
-		.post(diagnosticsURL)
-		.send({
-
-			"text": text,
-
-			"color": "#36a64f", // Can either be one of 'good', 'warning', 'danger', or any hex color code
-
-			// Fields are displayed in a table on the message
-
-			"fields": [
-				{
-					"title": "Command Fired", // The title may not contain markup and will be escaped for you
-					"value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
-					"short": false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
-				}
-			]
-
-		})
-		.end(function(err){
-			if(!err){
-				console.log('Sent without error', text);
-			}
-			console.log('ERR', err);
-
-			cb && cb()
-		});
-}
-
-
-
-
-sendDiagmsg("Server restart!");
+diag.msg("Server restart!");
 
 
 
@@ -143,7 +101,7 @@ var rollCmd = {
 
 process.on('SIGTERM', function (){
 	console.log('reacting to SIGTERM');
-	sendDiagmsg("Server shutting down for real... goodnight. :moon:", function(){
+	diag.msg("Server shutting down for real... goodnight. :moon:", function(){
 		process.exit(0)
 	});
 });
---
+++ shared/diag.js
@@ -0,0 +1,61 @@
+var _ = require('lodash');
+var request = require('superagent');
+
+var diagnosticsURL = process.env.DIAGNOSTICS_WEBHOOK;
+
+
+
+module.exports = {
+
+	//green
+	info : function(){
+
+	},
+
+	//blue
+	log : function(){
+
+	},
+
+	//red
+	error : function(){
+
+	},
+
+	//yellow
+	warn : function(){
+
+	},
+
+
+	msg : function(){
+		var args = Array.prototype.slice.call(arguments, 1);
+		var res = _.map(args, function(arg){
+			return JSON.stringify(arg);
+		}), join(', ');
+		request.post(diagnosticsURL)
+			.send({
+				"text" : res
+			});
+	},
+
+}
+
+/*{
+
+			"text": text,
+
+			"color": "#36a64f", // Can either be one of 'good', 'warning', 'danger', or any hex color code
+
+			// Fields are displayed in a table on the message
+
+			"fields": [
+				{
+					"title": "Command Fired", // The title may not contain markup and will be escaped for you
+					"value": "Text value of the field. May contain standard message markup and must be escaped as normal. May be multi-line.",
+					"short": false // Optional flag indicating whether the `value` is short enough to be displayed side-by-side with other values
+				}
+			]
+
+		}
+		*/
