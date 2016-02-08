8321cb9dfc85ef119caa446dc4b360e145b624a0
Jared Tyler <jared@thalmic.com>
2016-02-08 12:40:11 -0500

    Sending async errors so they're caught & reported properly.

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -8,14 +8,20 @@ module.exports = function(msg, info, reply){
 	google('site:xkcd.com ' + msg, function(err, nextPage, links) {
 		if (links.length < 1) {
 			// TODO: Tell user if their keywords resulted in absolutely nothing.
-			throw new Error('nawp');
+			return reply({
+				response_type : 'ephemeral',
+				text : 'nawp'
+			});
 		}
 		var url = links[0].href;
 		// TODO: check  if 'url' is NOT a direct comic page, then try next link
 		var matches = url.match(/^https?:\/\/xkcd.com\/([\d]+)\/?$/);
 		if (matches === null || matches.length < 2 || !matches[1]) {
 			// TODO: couldn't find match
-			throw new Error('nawp 2');
+			return reply({
+				response_type : 'ephemeral',
+				text : 'nawp 2'
+			});
 		}
 		// TODO: try
 		var comicId = Number(matches[1]);
