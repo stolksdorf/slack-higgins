a0036ff56824064dfc9148871d3249361769f7d5
Jared Tyler <jared@thalmic.com>
2016-02-08 13:57:03 -0500

    Cleaning up xkcd command and that's a wrap.

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -2,61 +2,58 @@ var _      = require('lodash');
 var google = require('google');
 var xkcd   = require('xkcd');
 
-module.exports = function(msg, info, reply){
+var ERR_NOT_FOUND = 'Could not find any comics using those keywords.  Try something else!';
+
+module.exports = function(msg, info, reply, error) {
 	google('site:xkcd.com ' + msg, function(err, nextPage, links) {
-		try {
-			if (links.length < 1) {
-				// TODO: Tell user if their keywords resulted in absolutely nothing.
-				return reply({
-					response_type : 'ephemeral',
-					text : 'nawp'
-				});
+		if (err) {
+			return error(err);
+		}
+
+		if (links.length < 1) {
+			return error(ERR_NOT_FOUND);
+		}
+
+		var foundComic = _.reduce(links, function(foundComic, link) {
+			if (foundComic) {
+				return foundComic;
 			}
-			var foundComic = _.reduce(links, function(foundComic, link) {
-				if (foundComic) {
-					return foundComic;
-				}
-				var matches = link.href.match(/^https?:\/\/xkcd.com\/([\d]+)\/?$/);
-				if (matches === null || matches.length < 2 || !matches[1]) {
-					// Link does not point to an explicit comic page, try the next one.
-					return false;
-				}
-				// TODO: try
+
+			var matches = link.href.match(/^https?:\/\/xkcd.com\/([\d]+)\/?$/);
+			if (matches === null || matches.length < 2 || !matches[1]) {
+				// Link does not point to an explicit comic page, try the next one.
+				return false;
+			}
+
+			try {
 				var comicId = Number(matches[1]);
-				xkcd(comicId, function(data) {
-					try {
-						// TODO: handle if no comic was found
-						return reply({
-							attachments: [
-								{
-									title : data.safe_title,
-									title_link : link.href,
-									image_url : data.img
-								}, {
-									text : '_' + data.alt + '_'
-								}
-							]
-						});
-					} catch (err) {
-						return reply({
-							response_type : 'ephemeral',
-							text : 'nawp 3 -- ' + link.href + ' -- ' + comicId
-						});
-					}
-				});
-				return true;
-			}, false);
-			if (!foundComic) {
-				return reply({
-					response_type : 'ephemeral',
-					text : 'nawp 2'
-				});
+			} catch (err) {
+				return error(new Error('Parsed out a comic ID but failed to cast to Number... ' + JSON.stringify(matches)));
 			}
-		} catch (err) {
-			return reply({
-				response_type : 'ephemeral',
-				text : 'nawp 4'
-			});
+
+			try {
+				xkcd(comicId, function(data) {
+					return reply({
+						attachments: [
+							{
+								title : data.safe_title,
+								title_link : link.href,
+								image_url : data.img
+							}, {
+								text : data.alt
+							}
+						]
+					});
+				});
+			} catch (err) {
+				return error(new Error('xkcd() threw an error; this should not really happen... ' + JSON.stringify(matches)));
+			}
+
+			return true;
+		}, false);
+
+		if (!foundComic) {
+			return error(ERR_NOT_FOUND);
 		}
 	});
 };
