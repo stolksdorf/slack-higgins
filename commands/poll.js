6434bebe0d49899f0b603ee69005498f4762a0ab
Scott <scott.tolksdorf@gmail.com>
2016-02-21 02:17:13 -0500

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,16 @@
+var _ = require('lodash');
+var Logbot = require('logbot');
+
+var nums = [':one:',':two:',':three:',':four:',':five:',':six:',':seven:',':eight:',':nine:'];
+
+module.exports = function(msg, info, reply, error){
+	var parts = msg.split('?');
+	var question = parts[0]
+	var options = parts[1].split(',');
+
+	reply('*' + question + '*\n\n' +
+		_.map(options, (opt, index)=>{
+			return nums[index] + ' ' + _.trim(opt);
+		}).join('\n')
+	);
+}
