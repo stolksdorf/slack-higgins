792d43356c9375e6220bf87b30a1c573352b724c
Kellen Steffen <kellen.steffen@thalmic.com>
2016-02-24 16:34:24 -0500

    Adding "or" to dumbWords.

--- bots/triviabot.js
+++ bots/triviabot.js
@@ -82,7 +82,7 @@ var stringToCleanWordArray = function(string) {
 
 var checkAnswer = function(msg){
 	if(!msg) return;
-	var dumbWords = ['the', 'their', 'sir', 'its', 'a', 'an', 'and'];
+	var dumbWords = ['the', 'their', 'sir', 'its', 'a', 'an', 'and', 'or'];
 
 	var msgWords = stringToCleanWordArray(msg);
 	var answerWords = stringToCleanWordArray(storedClue.answer);
