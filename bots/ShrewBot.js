c67c193011c4e167b6d96b3e1c3ffe8b99361107
Meggeroni <meg.kirkl@gmail.com>
2016-03-02 13:35:13 -0500

    Update ShrewBot.js

--- bots/ShrewBot.js
+++ bots/ShrewBot.js
@@ -1,20 +1,33 @@
 
+//wiggle the fraggle is a lodash?
 //var _ = require('lodash');
-//var shrew_emoji = ['sandshrew'];
 
+//define the emoji that I'm all about
+var shrew_emoji = ['sandshrew'];
+
+//define shrew messages
 var isShrewRequest = function(msg){
-	return _contains(msg, ['Sandshrew', 'choose','go','shrewbro']);
+	return _contains(msg, ['Sandshrew','choose','go','shrewbro']);
 	}
+
+//words of ancient voodoo provided by scott to whisper gently over my bot
+var _contains = function(str, list){
+	return _.some(list, (word)=>{
+		return _.includes(str.toLowerCase(), word.toLowerCase());
+	});
+}	
 	
+//random junk maybe? Will I need it someday, only time will tell.	
 //var isShrewDismiss = function(msg){
 //	return _contains(msg, ['Sandshrew good job', 'Sandshrew that's enough']);	
 
+//do the shit
 module.exports = {
 	listenFor : ['message'],	
 	response : function(msg, info, Higgins){
       		if(isShrewRequest(msg)){
     	  	if(info.user == 'meggeroni'){
-			  Higgins.react('sandshrew');
+			  Higgins.react(shrew_emoji);
     	  		}
 			}
 		}	
