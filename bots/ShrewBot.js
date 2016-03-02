a25da1d2b0943a9fe9cc92edf20f043bb2823439
Meggeroni <meg.kirkl@gmail.com>
2016-03-02 13:20:39 -0500

    Create ShrewBot.js

---
+++ bots/ShrewBot.js
@@ -0,0 +1,20 @@
+
+var _ = require('lodash');
+//var shrew_emoji = ['sandshrew'];
+
+var isShrewRequest = function(msg){
+	return _contains(msg, ['Sandshrew I choose you', 'Sandshrew go']);
+	}
+	
+//var isShrewDismiss = function(msg){
+//	return _contains(msg, ['Sandshrew good job', 'Sandshrew that's enough']);	
+
+module.exports = {
+	listenFor : ['message'],	
+
+      if(isShrewRequest(msg)){
+    	  if(info.user == 'meggeroni'){
+			  Higgins.react(sandshrew);
+			}
+		}	
+	};	
