6333c294aa8581fe031c3e135ba17b0846475a03
Meggeroni <meg.kirkl@gmail.com>
2016-03-03 21:38:50 -0500

    What's this? Shrewbot is evolving!

--- bots/ShrewBot.js
+++ bots/ShrewBot.js
@@ -5,6 +5,8 @@ var _ = require('lodash');
 //define the emoji that I'm all about. I don't think I need this
 //var shrew_emoji = ['sandshrew'];
 
+var isActive = false;
+
 //words of ancient voodoo provided by scott to whisper gently over my bot
 var _contains = function(str, list){
 	return _.some(list, (word)=>{
@@ -14,14 +16,13 @@ var _contains = function(str, list){
 
 //define shrew messages
 var isShrewRequest = function(msg){
-	return _contains(msg, ['choose','go']) && _contains(msg, ['Sandshrew','shrewbro']);
+	return _contains(msg, ['choose','go']) && _contains(msg, ['Sandshrew','shrewbro']) && !isActive;
 	};
 
-	
-	
 //random junk maybe? Will I need it someday, only time will tell.	
-//var isShrewDismiss = function(msg){
-//	return _contains(msg, ['Sandshrew good job', 'Sandshrew that's enough']);	
+var isShrewDismiss = function(msg){
+	return _contains(msg, ['enough','come back']) && _contains(msg, ['Sandshrew','shrewbro']) && isActive;	
+	};
 
 //do the shit
 module.exports = {
@@ -29,7 +30,14 @@ module.exports = {
 	response : function(msg, info, Higgins){
       		if(info.user == 'meggeroni' && isShrewRequest(msg)){
 			  Higgins.react('sandshrew');
+			  isActive = true;
     	  		}
-			
+		else if(isActive){
+			Higgins.react('sandshrew');	
+		}
+		else if(info.user == 'meggeroni' && isShrewDismiss(msg)){
+			  Higgins.react('pokeball');
+			  isActive = false;
+    	  		}
 		}	
-	};	
+};	
