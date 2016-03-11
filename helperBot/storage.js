815f201b4e53293c9d0b334d41dcdef8960a1a07
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-03-11 14:07:43 -0500

    Added temporary fallback storage solution

--- helperBot/storage.js
+++ helperBot/storage.js
@@ -2,16 +2,31 @@ var Logbot = require('./logbot');
 var redis = require("redis");
 var client = redis.createClient(process.env.REDIS_URL);
 
+var TEMP_STORAGE = {}
+
+
 client.on("error", function(err){
-	Logbot.error("Storage Error", err);
+	Logbot.error("Redis Storage Error", err);
+	Logbot.info("Falling back to temporary storage instance. ")
+	client.end();
+
+	//Fallback storage
+	client = {
+		get : function(key, cb){
+			cb(null, TEMP_STORAGE[key]);
+		},
+		set : function(key, val, cb){
+			TEMP_STORAGE[key] = val;
+			cb();
+		}
+	}
 });
 
 
 module.exports = {
 	get : function(key, cb){
 		return client.get(key, function(err, res){
-			if(err) return Logbot.error(err);
-			cb(JSON.parse(res));
+			if(!err) cb(JSON.parse(res));
 		})
 	},
 	set : function(key, val, cb){
