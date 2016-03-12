0cf73fcc5df7b26c970cc8af7b2e1f1b45608743
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-03-12 14:01:38 -0500

    Cleaned up the index

--- helperbot/index.js
+++ helperbot/index.js
@@ -6,35 +6,21 @@ var bots = require('./bots');
 
 module.exports = function(configObj){
 	logbot.setUrl(configObj.diagnosticsWebhook);
-
 	var cmdLoadResult = cmds.load(configObj.expressApp, configObj.cmdList)
 
-	console.log(cmdLoadResult);
-
-
-
 	bots.start(configObj.botInfo, configObj.local, configObj.debug);
-
 	var botLoadResult = bots.load(configObj.botList);
 
-
-	console.log(botLoadResult);
-
-
-	//TODO: Separate message for local testing
-
-	/*
+	//Separate message for local testing
 	if(configObj.local){
 		return logbot.info('Local Development Connected',
 			'Local version of ' + configObj.botInfo.name + ' has successfully connected to slack.');
 	}
-	*/
 
 	if(cmdLoadResult.error.length || botLoadResult.error.length){
-		console.log('WHAT');
 		return logbot.error('Server Failed Restart', 'There were some issues restarting the server, check logbot.');
 	}
 
-	logbot.info('Server Restart', 'Successfully loaded:\n > *Bots:* `woo.js` \n > *Cmds:* `neato.js`');
+	logbot.info('Server Restart', 'Successfully rebooted!');
 
 }
