9faa7d56cf474aba37e98162503ebbf62b0db9f5
Meggeroni <meg.kirkl@gmail.com>
2016-04-01 09:07:40 -0400

    Update prankbot.js

--- bots/prankbot.js
+++ bots/prankbot.js
@@ -1,8 +1,10 @@
+var jared_pester = ['HER JARED', 'WHATS UP JARED'];
+
 module.exports = {
   listenFor : ['user_typing'],
   response : function(msg, info, Higgins){
     if(info.user == 'meggeroni'){
-      Higgins.reply('meow');
+      Higgins.reply(_.sample(jared_pester));
     }
   }
 }
