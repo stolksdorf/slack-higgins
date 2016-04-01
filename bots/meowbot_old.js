1a0727e467ad1b68e0b3fe0e1d044e8e6a608928
Meggeroni <meg.kirkl@gmail.com>
2016-04-01 08:39:09 -0400

    Update meowbot_old.js

--- bots/meowbot_old.js
+++ bots/meowbot_old.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //listenFor : ['user_typing'],
+  listenFor : ['user_typing'],
   response : function(msg, info, Higgins){
     if(info.user == 'meggeroni'){
       Higgins.reply('meow');
