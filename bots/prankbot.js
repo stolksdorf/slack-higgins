ecb6e9db67c902ac960135518dcce97f1c8fcce5
Scott <scott.tolksdorf@gmail.com>
2016-04-01 23:16:05 -0400

    disabling the bot and adding a throttle to him if we re-enable him

--- bots/prankbot.js
+++ bots/prankbot.js
@@ -2,11 +2,10 @@ var _ = require('lodash');
 var jared_pester = ['HEY JARED', 'WHATCHYA THINKING ABOUT JARED','WHAT ARE YOUR THOUGHTS JARED','SPIT IT OUT JARED','T-T-T-TODAY JARED','YOU SURE ARE TYPING A LOT JARED','THE SUSPENSE IS KILLING ME JARED','CANT WAIT TO HEAR YOUR THOUGHTS JARED','WHATS UP JARED','ᶦ ᶠᵘᶜᵏᶦᶰᵍ ᶫᵒᵛᵉ ᶦᵗ ʷʰᵉᶰ ʸᵒᵘ ᵗʸᵖᵉ ʲᵃʳᵉᵈ'];
 
 module.exports = {
-  listenFor : ['user_typing'],
-  response : setInterval(function(msg, info, Higgins){
+  //listenFor : ['user_typing'],
+  response : _.throttle(function(msg, info, Higgins){
     if(info.user == 'jared'){
       Higgins.reply(_.sample(jared_pester));
     }
-  },
-  100)
+  }, 1000)
 }
