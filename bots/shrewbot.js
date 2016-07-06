f871ebf5ed3c6c252728f716687d4e20fd859071
Scott <scott.tolksdorf@gmail.com>
2016-07-06 16:44:36 -0400

    Create shrewbot.js

---
+++ bots/shrewbot.js
@@ -0,0 +1,10 @@
+var _ = require('lodash');
+
+module.exports = {
+	listenFor : ['message'],
+	response : function(msg, info, Higgins){
+		if(info.user == 'meggeroni' && msg.indexOf('sandshrew') != -1){
+			Higgins.react('sandshrew');
+		}
+	},
+};
