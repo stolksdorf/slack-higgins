0e373c87c7be480470218ae2ba4a800d24325962
Meg Kirkland <meg.kirkl@gmail.com>
2016-08-06 18:06:35 -0400

    Update prankbot.js

--- bots/prankbot.js
+++ bots/prankbot.js
@@ -1,10 +1,11 @@
 var _ = require('lodash');
-var jared_pester = ['HEY JARED', 'WHATCHYA THINKING ABOUT JARED','WHAT ARE YOUR THOUGHTS JARED','SPIT IT OUT JARED','T-T-T-TODAY JARED','YOU SURE ARE TYPING A LOT JARED','THE SUSPENSE IS KILLING ME JARED','CANT WAIT TO HEAR YOUR THOUGHTS JARED','WHATS UP JARED','ᶦ ᶠᵘᶜᵏᶦᶰᵍ ᶫᵒᵛᵉ ᶦᵗ ʷʰᵉᶰ ʸᵒᵘ ᵗʸᵖᵉ ʲᵃʳᵉᵈ'];
+var jared_pester = ['HEY CATHLEEN', 'WHATCHYA THINKING ABOUT CATHLEEN','WHAT ARE YOUR THOUGHTS CATHLEEN','SPIT IT OUT CATHLEEN','T-T-T-TODAY CATHLEEN','YOU SURE ARE TYPING A LOT CATHLEEN','THE SUSPENSE IS KILLING ME CATHLEEN','CANT WAIT TO HEAR YOUR THOUGHTS CATHLEEN','WHATS UP CATHLEEN'];
+//,'ᶦ ᶠᵘᶜᵏᶦᶰᵍ ᶫᵒᵛᵉ ᶦᵗ ʷʰᵉᶰ ʸᵒᵘ ᵗʸᵖᵉ ʲᵃʳᵉᵈ'
 
 module.exports = {
-  //listenFor : ['user_typing'],
+  listenFor : ['user_typing'],
   response : _.throttle(function(msg, info, Higgins){
-    if(info.user == 'jared'){
+    if(info.user == 'gobot'){
       Higgins.reply(_.sample(jared_pester));
     }
   }, 1000)
