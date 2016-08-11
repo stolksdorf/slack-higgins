dbb3f9f300597d16cc3b3ad990977d8df8f8d648
Meg Kirkland <meg.kirkl@gmail.com>
2016-08-11 12:35:30 -0400

    Update shrewbot.js

--- bots/shrewbot.js
+++ bots/shrewbot.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 
 module.exports = {
-	listenFor : ['message'],
+//	listenFor : ['message'],
 	response : function(msg, info, Higgins){
 		if(info.user == 'meggeroni' && msg.indexOf('sandshrew') != -1){
 			Higgins.react('sandshrew');
