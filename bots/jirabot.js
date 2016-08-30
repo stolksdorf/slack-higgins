d1446ccf04e7b84126312f9f0a7dc6601fd8b0b9
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-08-30 16:14:28 -0400

    Update jirabot.js

--- bots/jirabot.js
+++ bots/jirabot.js
@@ -6,8 +6,14 @@ module.exports = {
 		if(info.user == 'scott'){
 			var projects = ['KCAA', 'KLOUD', 'TIMS', 'TLWEB'];
 
-      var projectRegex = new RegExp('((' + projects.join('|') + ')-([0-9]{1,4}))', 'ig');
-      Higgins.reply('`' + JSON.stringify(msg.match(projectRegex)) + '`');
+			var projectRegex = new RegExp('((' + projects.join('|') + ')-([0-9]{1,4}))', 'ig');
+			var matches = msg.match(projectRegex);
+			if(matches){
+				_.each(matches, (match) => {
+					msg = msg.replace(match, `https://jira.thalmiclabs.com/browse/${match.toUpperCase()}`)
+				});
+				Higgins.reply(msg);
+			}
 		}
 	},
 };
