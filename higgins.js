fbe17f29e141616156b2b9463332a2c74e2b98d4
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-09-07 16:14:28 -0400

    'Adding

--- higgins.js
+++ higgins.js
@@ -50,8 +50,83 @@ glob('./cmds/**/*.cmd.js', {}, (err, files) => {
 		}
 		return r;
 	}, []);
-	Higgins.loadCmds(app, Cmds);
+	//Higgins.loadCmds(app, Cmds);
+	test.load(app, Cmds);
 });
+
+
+
+
+
+
+
+
+
+const formatResponse = (response) => {
+	if(_.isString(response)){
+		return { text : response };
+	}else if(!_.isPlainObject(response)){
+		return { text : JSON.stringify(response) };
+	}else{
+		return response;
+	}
+};
+
+var test = {
+	load : function(expressInstance, cmds){
+		_.each(cmds, (cmd) => {
+			if(!cmd.url){
+				console.error('URL not set for command');
+				return;
+			}
+
+			console.log('adding', cmd.url);
+
+			expressInstance.all(`${cmd.url}`, (req, res) => {
+				console.log('processing endpoint');
+				const input = _.assign({}, req.query, req.body);
+				try{
+					cmd.handle(
+						input.text,
+						input,
+						(response) => {
+							return res.status(200).send(_.assign({
+								'response_type': 'in_channel',
+							}, formatResponse(response)));
+						},
+						(error) => {
+							return res.status(200).send(_.assign({
+								'response_type': 'ephemeral',
+							}, formatResponse(err)));
+						}
+					);
+				}catch(err){
+					console.error(`Command Run Error : ${err}`);
+					return res.status(200).send();
+				}
+			})
+		});
+		return expressInstance;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res)=>{
