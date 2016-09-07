2b391b65b01a8a2296de32516f06dde6b28c7b99
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-09-07 13:53:43 -0400

    someting about shrewbot

--- bots/shrewbot.js
+++ bots/shrewbot.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 
 module.exports = {
 //	listenFor : ['message'],
+
 	response : function(msg, info, Higgins){
 		if(info.user == 'meggeroni' && msg.indexOf('sandshrew') != -1){
 			Higgins.react('sandshrew');
