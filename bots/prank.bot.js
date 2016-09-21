b45b615688db2e6e0c2c72c4a2c7064fb65b3551
Meg Kirkland <meg.kirkl@gmail.com>
2016-09-21 10:15:36 -0400

    Update prank.bot.js

--- bots/prank.bot.js
+++ bots/prank.bot.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 var jared_pester = ['HEY CATHLEEN', 'WHATCHYA THINKING ABOUT CATHLEEN','WHAT ARE YOUR THOUGHTS CATHLEEN','SPIT IT OUT CATHLEEN','T-T-T-TODAY CATHLEEN','YOU SURE ARE TYPING A LOT CATHLEEN','THE SUSPENSE IS KILLING ME CATHLEEN','CANT WAIT TO HEAR YOUR THOUGHTS CATHLEEN','WHATS UP CATHLEEN'];
 //,'ᶦ ᶠᵘᶜᵏᶦᶰᵍ ᶫᵒᵛᵉ ᶦᵗ ʷʰᵉᶰ ʸᵒᵘ ᵗʸᵖᵉ ʲᵃʳᵉᵈ'
 
-module.exports = {
+/*module.exports = {
   channel : '*',
   handle : _.throttle(function(msg, info, Higgins){
     if(info.user == 'gobot'){
@@ -10,3 +10,4 @@ module.exports = {
     }
   }, 1000)
 }
+*/
