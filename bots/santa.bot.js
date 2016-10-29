8f571a42ec0f9c71349949df0981505601f40d8d
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-10-29 19:09:07 -0400

    Create santa.bot.js

---
+++ bots/santa.bot.js
@@ -0,0 +1,11 @@
+var utils = require('slack-microbots/utils');
+
+module.exports = {
+	name : 'santabot',
+	icon : ':santa:',
+	channel : '*',
+	handle : function(msg, info, Higgins){
+    if(info.isDirect && utils.messageHas('santachat')){
+      Higgins.reply(msg, 'secret-santa');
+    }
+}
