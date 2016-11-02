7c6deb8a5b3a80a3334fd77ca461e93158133525
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-11-02 16:07:02 -0400

    Update santa.bot.js

--- bots/santa.bot.js
+++ bots/santa.bot.js
@@ -6,7 +6,12 @@ module.exports = {
 	channel : '*',
 	handle : function(msg, info, Higgins){
 		if(info.isDirect && utils.messageHas(['santachat', 'santabot'])){
-			Higgins.reply(msg.replace('santachat', '').replace('santabot', ''), 'secret-santa');
+			Higgins.reply(msg
+				.replace('santachat', '')
+				.replace('santabot', '')
+				.replace('Santachat', '')
+				.replace('Santabot', '')
+			, 'secret-santa');
 		}
 	}
 }
