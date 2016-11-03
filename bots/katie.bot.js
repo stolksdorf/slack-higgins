5bdac11d0106aeee3a68cb644128fedfea33c8ef
McCsocks <mccann.ek@gmail.com>
2016-11-02 21:16:21 -0400

    Update katie.bot.js

--- bots/katie.bot.js
+++ bots/katie.bot.js
@@ -7,9 +7,9 @@ module.exports = {
 	name : 'katiebot',
 	icon : ':katie:',
 	channel : 'secret-laboratory',
-	handle : _.delay(function(msg, info, Higgins){
+	handle : function(msg, info, Higgins){
     			if(info.user == 'katie'){
-				Higgins.reply(_.sample(santaSayings));
+				_.delay(Higgins.reply(_.sample(santaSayings)), 5000);
     		}
-	}, 1000)
+	}
 };
