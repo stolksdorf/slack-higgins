a7f6410c7c5194f2feb272637c4bd605a035cc87
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-05-20 00:20:13 -0400

    pizza scoring is working

--- bots/pizza.engine.js
+++ bots/pizza.engine.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const sizes = {
 	small : 6,
 	med   : 8,
-	large : 12
+	large : 10
 };
 
 
@@ -15,10 +15,10 @@ const PizzaEngine = {
 		}
 	},
 
-	getPizzaSet: (toppings, slices = 0)=>{
+	getPizzaSet: (toppings, slicesNeeded = 10)=>{
 		let total = 0;
 		let result = [];
-		while(total < slices){
+		while(total < slicesNeeded){
 			const pizza = PizzaEngine.getRandomPizza(toppings);
 			total += sizes[pizza.size];
 			result.push(pizza);
@@ -27,14 +27,84 @@ const PizzaEngine = {
 	},
 
 
-	scorePizza: (pizza, prefs)=>{
-		//for each person
-			//find pizza pref,
-			//eat relative number of slices
-			//points per slice eaten
-			//
+	scorePizzaSet: (pizzas, people)=>{
+		getBias = (pizzas, people)=>{
+			let result = _.times(pizzas.length, ()=>{return {}});
+			return _.mapValues(people, (prefs, peep)=>{
+				const dispositions = _.map(pizzas, (pizza)=>{
+					return _.reduce(pizza.toppings, (acc, topping)=>{
+						if(acc == 0) return acc;
+						if(_.includes(prefs.like, topping)) return acc + 1;
+						if(_.includes(prefs.hate, topping)) return 0;
+						return acc;
+					},1)
+				});
+				const total = _.sum(dispositions);
+				return _.map(dispositions, (disposition, idx)=>{
+					result[idx][peep] = {
+						bias : disposition,
+						slicesToEat : (total ? prefs.hunger * disposition / total : 0),
+						hunger : prefs.hunger
+					};
+					return {
+						bias : disposition,
+						slicesToEat : (total ? prefs.hunger * disposition / total : 0)
+					};
+				});
+			});
+			return result;
+		}
 
+		let score = 0;
+		const peopleBias = getBias(pizzas, people);
+		let slicesRemaining = _.map(pizzas, (pizza)=>sizes[pizza.size]);
+		let hungerRemaining = _.mapValues(people, (prefs)=>prefs.hunger);
+
+		_.each(peopleBias, (pizzaPrefs, peep)=>{
+			_.each(pizzaPrefs, (pref, pizzaIdx)=>{
+				// console.log('-------');
+				// console.log(peep, pref, pizzaIdx);
+				const slices = _.min([slicesRemaining[pizzaIdx], pref.slicesToEat])
+				slicesRemaining[pizzaIdx] -= slices;
+				hungerRemaining[peep] -= slices;
+				score += slices*pref.bias;
+				//console.log(score, slicesRemaining, hungerRemaining);
+			});
+		});
+
+		//Reduce score if anyone is hungry
+		score -= _.sum(_.values(hungerRemaining)) * 5;
+		//Reduce score for 'wasted' pizza
+		score -= _.sum(slicesRemaining) * 2
+		return score;
 	},
+
+	getScoredPizzaSet : (slices, toppings, prefs)=>{
+		const pizzaSet = PizzaEngine.getPizzaSet(toppings, slices);
+		return {
+			score : PizzaEngine.scorePizzaSet(pizzaSet, prefs),
+			pizzaSet
+		}
+	},
+
+	getOptimalPizzaSet : (prefs, attempts = 10)=>{
+		const toppings = _.chain(prefs)
+			.map((pref)=>pref.like)
+			.flatten()
+			.uniq()
+			.value();
+		const totalSlices = _.reduce(prefs, (acc, pref)=>acc+pref.hunger, 0);
+
+
+		const results = _.times(attempts, ()=>PizzaEngine.getScoredPizzaSet(totalSlices, toppings, prefs));
+
+		return _.sortBy(results, (res)=>-res.score);
+
+
+
+
+
+	}
 
 
 
--- bots/pizza.test.js
+++ bots/pizza.test.js
@@ -3,50 +3,59 @@ const _ = require('lodash');
 const PizzaEngine = require('./pizza.engine.js');
 
 
-const toppings = [
-	'red_circle',
-	'mushroom',
-	'bell',
-	'tomato',
-	'cheese',
-	'hot_pepper',
-	'pineapple',
-	'chicken',
-	'cow2',
-	'pig2',
-	'hotdog',
-
-	'garlic',
-	'onion',
-	'olive',
-	'olive_black'
-];
+const toppings = ['red_circle', 'mushroom', 'bell', 'tomato', 'cheese', 'hot_pepper', 'pineapple', 'chicken', 'cow2', 'pig2', 'hotdog', 'garlic', 'onion', 'olive', 'olive_black'];
 
 const prefs = {
 	scott : {
 		like : ['garlic', 'onion', 'chicken'],
 		hate : ['olive', 'olive_black', 'hot_pepper'],
-		hunger : 4
+		hunger : 5
 	},
 	meg : {
 		like : ['red_circle', 'olive', 'chicken'],
-		hate : ['onion', 'pineapple'],
+		hate : ['pineapple'],
 		hunger : 8
+	},
+	katie : {
+		like : [],
+		hate : ['pineapple'],
+		hunger : 2
 	}
 }
 
-const testpizza1={
-	size : 'med',
-	toppings: ['olive', 'onion']
-}
-const testpizza2={
-	size : 'large',
-	toppings: ['chicken', 'onion', 'garlic']
-}
+const testSet1 =[
+	{
+		size : 'med',
+		toppings: ['olive', 'onion']
+	},
+	{
+		size : 'large',
+		toppings: ['chicken', 'onion', 'garlic']
+	}
+];
 
-console.log(PizzaEngine.getRandomPizza(toppings));
+const testSet2 =[
+	{
+		size : 'large',
+		toppings: ['olive', 'onion']
+	},
+	{
+		size : 'large',
+		toppings: ['chicken', 'onion', 'garlic']
+	}
+];
 
-console.log(PizzaEngine.getPizzaSet(toppings, 10));
+//console.log(PizzaEngine.getRandomPizza(toppings));
+
+//console.log(PizzaEngine.getPizzaSet(toppings, 10));
+
+// console.log('Get Pizza bias');
+// console.log(PizzaEngine.scorePizzas(testSet1, prefs));
+// console.log(PizzaEngine.scorePizzas(testSet2, prefs));
+
+
+const res = PizzaEngine.getOptimalPizzaSet(prefs)
+console.log(JSON.stringify(res, null, '  '));
 
 
 
