49666859cb865e4a4644a3da7a7534afa8a26f55
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-06-01 14:52:11 -0400

    Update conspiracy.bot.js

--- bots/conspiracy.bot.js
+++ bots/conspiracy.bot.js
@@ -7,6 +7,8 @@ Slack.onMessage((msg)=>{
   //Lock this to just direct messages from chris
 	if(!msg.isDirect || msg.user != 'chris') return;
 	
+	Slack.log('Why hello!');
+	
   if(Slack.msgHas(msg.text, 'conspiracy?')){
     Slack.msgAs('conspiracybot', ':sleuth_or_spy:', msg.channel, _.sample(responses));
   }
