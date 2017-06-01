c96606fa6b1efef194b0b1d47db19eeb308aedfa
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-06-01 14:51:09 -0400

    Create conspiracy.bot.js

---
+++ bots/conspiracy.bot.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+const Slack = require('pico-slack');
+
+const responses = ['my my', 'why yes!'];
+
+Slack.onMessage((msg)=>{
+  //Lock this to just direct messages from chris
+	if(!msg.isDirect || msg.user != 'chris') return;
+	
+  if(Slack.msgHas(msg.text, 'conspiracy?')){
+    Slack.msgAs('conspiracybot', ':sleuth_or_spy:', msg.channel, _.sample(responses));
+  }
+
+});
