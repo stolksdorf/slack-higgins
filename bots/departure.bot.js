775d29d4abddecfe55e5b6f1548ecbd1c17df0e3
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-06-11 19:00:07 -0400

    Added departure bot

---
+++ bots/departure.bot.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+const Slack = require('pico-slack');
+
+const quotes = [
+	'_Man’s feelings are always purest and most glowing in the hour of meeting and of farewell._',
+	'_Gone — flitted away,\nTaken the stars from the night and the sun\nFrom the day!\nGone, and a cloud in my heart._',
+	'_How lucky I am to have something that makes saying goodbye so hard._',
+	'_Man’s feelings are always purest and most glowing in the hour of meeting and of farewell_',
+	'_Don’t be dismayed at goodbyes. A farewell is necessary before you can meet again. And meeting again, after moments or lifetime, is certain for those who are friends._',
+	'_May the road rise up to meet you, may the wind be ever at your back. May the sun shine warm upon your face and the rain fall softly on your fields. And until we meet again, may God hold you in the hollow of his hand._',
+	'_Be well, do good work, and keep in touch._',
+	'_But fate ordains that dearest friends must part._',
+	'_As contraries are known by contraries, so is the delight of presence best known by the torments of absence._',
+	'_The return makes one love the farewell._',
+	'_I wanted a perfect ending. Now I’ve learned, the hard way, that some poems don’t rhyme, and some stories don’t have a clear beginning, middle, and end. Life is about not knowing, having to change, taking the moment and making the best of it, without knowing what’s going to happen next._',
+];
+
+
+Slack.emitter.on('member_left_channel', (data) => {
+	Slack.msg(data, _.sample(quotes));
+});
