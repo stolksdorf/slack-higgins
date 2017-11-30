25e679bcdcce01cac80f105307ad8ffe9d14b515
Chris Howlett <33726551+chrishowlett@users.noreply.github.com>
2017-11-29 21:04:45 -0500

    Update jester.bot.js

--- bots/jester.bot.js
+++ bots/jester.bot.js
@@ -1,10 +1,10 @@
 const Slack = require('pico-slack');
 
-const myFunnyJoke = {
-	triggers: ['feeling really triggered', 'ilubb']
-	joke: ['ye', 'ye ye', 'ye ye ye']
-	who: ['chris']
-}
+// const myFunnyJoke = {
+// 	triggers: ['feeling really triggered', 'ilubb']
+// 	joke: ['ye', 'ye ye', 'ye ye ye']
+// 	who: ['chris']
+// }
 
 const ohHai = (msg)=>{
 	if(Slack.msgHas(msg, ['higgins', 'higgs', 'higgs boson', 'higgerino'], ['hi', 'hello', 'wut up', 'mmm', 'ye'])){
