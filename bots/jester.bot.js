e778b8fe1ce5e68dee9c7ab3d91f3e7dad8ab28d
Chris Howlett <33726551+chrishowlett@users.noreply.github.com>
2017-11-29 20:56:52 -0500

    added a joke template

--- bots/jester.bot.js
+++ bots/jester.bot.js
@@ -1,5 +1,11 @@
 const Slack = require('pico-slack');
 
+const myFunnyJoke = {
+	triggers: ['feeling really triggered', 'ilubb']
+	joke: ['ye', 'ye ye', 'ye ye ye']
+	who: ['chris']
+}
+
 const ohHai = (msg)=>{
 	if(Slack.msgHas(msg, ['higgins', 'higgs', 'higgs boson', 'higgerino'], ['hi', 'hello', 'wut up', 'mmm', 'ye'])){
 		Slack.send(msg, `Up yours ${msg.user}`);
