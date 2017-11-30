912659b5dc0745b0f41f56e3e60a6505de03c1d1
Chris Howlett <33726551+chrishowlett@users.noreply.github.com>
2017-11-29 21:26:51 -0500

    hook me up with those thigh slappers

--- bots/jester.bot.js
+++ bots/jester.bot.js
@@ -1,14 +1,37 @@
 const Slack = require('pico-slack');
+const _ = require('lodash');
 
-// const myFunnyJoke = {
-// 	triggers: ['feeling really triggered', 'ilubb']
-// 	joke: ['ye', 'ye ye', 'ye ye ye']
-// 	who: ['chris']
-// }
+const jokes = [
+	{
+	triggers: ['feeling really triggered', 'ilu bb'],
+	joke: 'ye',
+	who: ['chris']
+	},
+	{
+	triggers: 'make me a sandwich',
+	joke: ['Poof! You\'re a sandwich!'],
+	who: ['chris']
+	},
+	{
+	triggers: ['exhausted'],
+	joke: 'Last night I dreamed I was a muffler, when I woke up this morning I was exhausted.',
+	who: ['chris']
+	},
+];
+
+const findJoke = (text)=>{
+	return _.find(jokes, (joke)=>{
+		retutn Slack.msgHas(text, joke.triggers);
+	})
+}
 
 const ohHai = (msg)=>{
-	if(Slack.msgHas(msg, ['higgins', 'higgs', 'higgs boson', 'higgerino'], ['hi', 'hello', 'wut up', 'mmm', 'ye'])){
-		Slack.send(msg, `Up yours ${msg.user}`);
+	//if(Slack.msgHas(msg, ['higgins', 'higgs', 'higgs boson', 'higgerino'], ['hi', 'hello', 'wut up', 'mmm', 'ye'])){
+	//	Slack.send(msg, `Up yours ${msg.user}`);
+	//}
+	const joke = findJoke(msg.text)
+	if(joke){
+		Slack.sendAs('JokeBot', ':chris:', msg, joke.joke);
 	}
 }
 
