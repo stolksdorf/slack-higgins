debb6116684927879f61c9d0178aef1823687186
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-12-13 22:45:43 -0500

    Update stream.bot.js

--- bots/stream.bot.js
+++ bots/stream.bot.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const Slack = require('pico-slack');
+
+let alreadySent = {};
+
+const isPlaying = (name)=>{
+	if(alreadySent[name]) return;
+	alreadySent[name] = true;
+	setTimeout(()=>alreadySent[name] = false, 1000 * 60 * 30);
+	Slack.sendAs('streambot', ':overwatch:', 'overwatch', 
+		     `*${name}* just started streaming. https://www.twitch.tv/${name}`
+	);
+};
 router.get('/streambot/:name', (req, res)=>{
-	Slack.sendAs('streambot', ':overwatch:', 'overwatch', `*${req.params.name}* just started streaming. https://www.twitch.tv/${req.params.name}`);
+	isPlaying(req.params.name);	
 	return res.send('ok');
-})
+});
+
 module.exports = router;
