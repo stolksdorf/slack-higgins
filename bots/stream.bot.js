2711fa58a8e67faf4871e30a32e3744344f6c2de
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-12-30 12:46:29 -0500

    Updating stream bot

--- bots/stream.bot.js
+++ bots/stream.bot.js
@@ -4,20 +4,26 @@ const Slack = require('pico-slack');
 const MIN = 1000 * 60;
 
 let timers = {};
-const check = (name)=>{
+const check = (name, game='overwatch')=>{
 	if(!timers[name]){
-		Slack.sendAs('streambot', ':overwatch:', 'overwatch', 
-			`*${name}* just started streaming. https://www.twitch.tv/${name}`
-		);
+		if(game == 'overwatch'){
+			Slack.sendAs('streambot', ':overwatch:', 'overwatch',
+				`*${name}* just started streaming Overwatch. https://www.twitch.tv/${name}`
+			);
+		} else{
+			Slack.sendAs('streambot', ':video_game:', 'vidya',
+				`*${name}* just started streaming ${game}. https://www.twitch.tv/${name}`
+			);
+		}
 	}
 	clearTimeout(timers[name]);
 	timers[name] = setTimeout(()=>{
 		timers[name] = false;
-	}, 45 * MIN);
+	}, 30 * MIN);
 };
 
-router.get('/streambot/:name', (req, res)=>{
-	check(req.params.name);
+router.get('/streambot/:name/:game', (req, res)=>{
+	check(req.params.name, req.params.game);
 	return res.send('ok');
 });
 
