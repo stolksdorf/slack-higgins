2a4b85c507891c2463641b02c710d2932c09f22b
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-02-27 09:45:00 -0500

    re-enabling bardbot

--- bots/bard.bot.js
+++ bots/bard.bot.js
@@ -1,36 +1,36 @@
 const _ = require('lodash');
-// const fetch = require('node-fetch');
-// const Slack = require('pico-slack');
+const fetch = require('node-fetch');
+const Slack = require('pico-slack');
 
-// const getWords = async (constraint, word, topic) => {
-//   const response = await fetch('https://api.datamuse.com/words?' + constraint + word +'&qe=sp&md=p&max=1')
-//   const data = await response.json();
-//   //Slack.log(data);
-//   return data[0].word;
-// }
+const getWords = async (constraint, word, topic) => {
+	const response = await fetch('https://api.datamuse.com/words?' + constraint + word +'&qe=sp&md=p&max=1')
+	const data = await response.json();
+	//Slack.log(data);
+	return data[0].word;
+}
 
-// const makePoem = async (text)=>{
-//   const clean = _.split(text, ' ');
-//   const trigger = _.last(clean);
+const makePoem = async (text)=>{
+	const clean = _.split(text, ' ');
+	const trigger = _.last(clean);
 
-//   const words = await Promise.all([
-//     getWords('rel_syn=', trigger),
-//     getWords('rel_gen=', trigger),
-//     getWords('rel_rhy=', trigger)
-//   ]);
-//   const rhymes = await Promise.all([
-//     getWords('rel_rhy=', words[1])
-//   ]);
-//   const poem = [trigger, 'is as the', words[0], 'or the', words[1], '\n', 'even', words[2], 'cannot compare to the', rhymes[0]].join(' ');
-//   return poem;
-// };
+	const words = await Promise.all([
+		getWords('rel_syn=', trigger),
+		getWords('rel_gen=', trigger),
+		getWords('rel_rhy=', trigger)
+	]);
+	const rhymes = await Promise.all([
+		getWords('rel_rhy=', words[1])
+	]);
+	const poem = [trigger, 'is as the', words[0], 'or the', words[1], '\n', 'even', words[2], 'cannot compare to the', rhymes[0]].join(' ');
+	return poem;
+};
 
-// const response = (msg)=>{
-//   if(!Slack.msgHas(msg.text, 'bardbot', 'poem')) return;
+const response = (msg)=>{
+	if(!Slack.msgHas(msg.text, 'bardbot', 'poem')) return;
 
-//   const poem = makePoem(msg.text)
-//   Slack.sendAs('BardBot', ':rose:', msg, poem);
-//   Slack.log(msg, msg.text, poem);
-// }
+	const poem = makePoem(msg.text)
+	Slack.sendAs('BardBot', ':rose:', msg, poem);
+	Slack.log(msg, msg.text, poem);
+}
 
-// Slack.onMessage(response);
+Slack.onMessage(response);
