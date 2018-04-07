6b3bcffd099422b8f2ee06660365366c9b5b9a4b
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-04-07 15:53:59 -0400

    Adding in a bot populate

--- bots/markovbot/markov.bot.js
+++ bots/markovbot/markov.bot.js
@@ -2,6 +2,8 @@ const _ = require('lodash');
 const Slack = require('pico-slack');
 const MarkovService = require('./markov.service.js');
 
+const Populate = require('./populate.js')
+
 const aliases = {
 	dave  : 'david',
 	meg   : 'meggeroni',
@@ -14,27 +16,39 @@ const aliases = {
 };
 const blacklist = [];
 
+const botSend = async (channel, user)=>{
+	const msg = await MarkovService.getNewMessage(user);
+	Slack.api('chat.postMessage', {
+		channel     : channel,
+		username    : `${user}bot`,
+		icon_emoji  : `:${user}:`,
+		attachments : JSON.stringify([{
+			pretext   : msg.text,
+			mrkdwn_in : ['pretext'],
+			footer    : `built with ${msg.msgs} messages, using ${msg.letters} letters.`
+		}])
+	});
+};
+
 Slack.onMessage((msg)=>{
+	if(msg.text == 'populate' && msg.user == 'scott'){
+		return Populate();
+	}
+
 	if(!msg.isDirect && !blacklist.includes(msg.channel)){
 		MarkovService.addMessage(msg.user, msg.text);
 	}
-	const send = async (user)=>{
-		const newMsg = await MarkovService.getNewMessage(user);
-		Slack.api('chat.postMessage', {
-			channel     : msg.channel,
-			username    : `${user}bot`,
-			icon_emoji  : `:${user}:`,
-			attachments : JSON.stringify([{
-				pretext   : newMsg.text,
-				mrkdwn_in : ['pretext'],
-				footer    : `built with ${newMsg.msgs} messages, using ${newMsg.letters} letters.`
-			}])
-		});
-	};
 	_.map(Slack.users, (user)=>{
-		if(Slack.msgHas(msg.text, `${user}bot`)) send(user);
+		if(Slack.msgHas(msg.text, `${user}bot`)) botSend(msg.channel, user);
 	});
 	_.map(aliases, (realName, alias)=>{
-		if(Slack.msgHas(msg.text, `${alias}bot`)) send(realName);
+		if(Slack.msgHas(msg.text, `${alias}bot`)) botSend(msg.channel, realName);
 	});
 });
+
+/** Random Proc **/
+const HOURS = 1000 * 60 * 60;
+const sendRandomMessage = ()=>{
+	botSend('bottin-around', _.sample(Slack.users));
+	setTimeout(sendRandomMessage, _.random(5, 10) * HOURS);
+}
