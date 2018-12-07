f8e7bc74be318af06e87f1b398af013664c9b8ed
Meg Kirkland <meg.kirkl@gmail.com>
2018-12-07 13:15:36 -0500

    Create battleshit.bot.js
    here goes nothin

---
+++ bots/battleshit.bot.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+const Slack = require('pico-slack');
+
+Slack.onMessage((msg)=>{
+	if(Slack.msgHas(msg.text, ['battleship', 'battleshit']){
+		Slack.react(msg, boat);
+    Slack.react(msg, hankey);
+	}
+});
