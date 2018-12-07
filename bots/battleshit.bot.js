38b2eea5c6fdbdc22d494dd1c12c3084cf51046c
Meg Kirkland <meg.kirkl@gmail.com>
2018-12-07 13:17:24 -0500

    Update battleshit.bot.js

--- bots/battleshit.bot.js
+++ bots/battleshit.bot.js
@@ -3,7 +3,7 @@ const Slack = require('pico-slack');
 
 Slack.onMessage((msg)=>{
 	if(Slack.msgHas(msg.text, ['battleship', 'battleshit']){
-		Slack.react(msg, boat);
-    Slack.react(msg, hankey);
+		Slack.react(msg, 'boat');
+    Slack.react(msg, 'hankey');
 	}
 });
