91c5f356d2a0a9d213b673bd4ab55b074dc470b0
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-12-07 15:31:49 -0500

    Fix to battleshit bot and running scripts

--- bots/battleshit.bot.js
+++ bots/battleshit.bot.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const Slack = require('pico-slack');
 
 Slack.onMessage((msg)=>{
-	if(Slack.msgHas(msg.text, ['battleship', 'battleshit']){
+	if(Slack.msgHas(msg.text, ['battleship', 'battleshit'])){
 		Slack.react(msg, 'boat');
     Slack.react(msg, 'hankey');
 	}
--- bots/self.bot.js
+++ bots/self.bot.js
@@ -1,12 +1,14 @@
 const Slack = require('pico-slack');
 const between = (str, term1, term2)=>str.substring(str.indexOf(term1) + term1.length, str.indexOf(term2)).trim();
 
+
+//eg. as coolbot in general : Hey guys!
 Slack.onMessage(async (msg)=>{
 	const lower = msg.text.toLowerCase();
 	if(msg.isDirect){
 		if(lower.startsWith('as') && lower.indexOf(':') !== -1){
 			const peep = between(lower, 'as ', ' in ');
-			const channel = between(lower, ' in ', ':');
+			const channel = between(lower, ' in ', ' ');
 			const message = msg.text.split(':')[1].trim();
 			Slack.sendAs(`${peep}bot`, peep, channel, message);
 		}
