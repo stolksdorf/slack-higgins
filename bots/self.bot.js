b6c203e5e00902ec94e6861e748f11145541004a
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-12-07 18:27:26 -0500

    fixing selfbot

--- bots/self.bot.js
+++ bots/self.bot.js
@@ -1,5 +1,9 @@
 const Slack = require('pico-slack');
-const between = (str, term1, term2)=>str.substring(str.indexOf(term1) + term1.length, str.indexOf(term2)).trim();
+const between = (str, term1, term2)=>{
+	const idx1 = str.indexOf(term1)+ term1.length;
+	const idx2 = str.indexOf(term2, idx1);
+	return str.substring(idx1, idx2).trim();
+}
 
 
 //eg. as coolbot in general : Hey guys!
@@ -8,9 +12,16 @@ Slack.onMessage(async (msg)=>{
 	if(msg.isDirect){
 		if(lower.startsWith('as') && lower.indexOf(':') !== -1){
 			const peep = between(lower, 'as ', ' in ');
-			const channel = between(lower, ' in ', ' ');
+			const channel = between(lower, ' in ', ':');
 			const message = msg.text.split(':')[1].trim();
-			Slack.sendAs(`${peep}bot`, peep, channel, message);
+			Slack.sendAs(peep, peep.replace('bot', ''), channel, message)
+				.then(()=>{
+					Slack.send(msg.user, `${peep}\n${channel}\n${message}`)
+				})
+				.catch((err)=>{
+					Slack.send(msg.user, `${peep}\n${channel}\n${message}`)
+					Slack.send(msg.user, err.toString())
+				})
 		}
 	}
 });
