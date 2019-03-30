27b695ea191cbf3877243d59201a1c90d2679346
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-03-29 22:09:20 -0400

    Crime bot is now updated

--- bots/crime.bot.js
+++ bots/crime.bot.js
@@ -1,42 +1,22 @@
 const config = require('pico-conf');
 const Slack = require('pico-slack');
-const request = require('superagent');
+const Gist = require('pico-gist')(config.get('github_token'));
 
 const crimeGistID = '24d22dfeecc3d02e419cad8739e8b672';
-const gistName= '20crimeteen.txt';
-const token = config.get('github_token');
 
-const updateCrimeGist = async (newContent)=>{
-	return request.get(`https://api.github.com/gists/${crimeGistID}`)
-		.set('Accept', 'application/vnd.github.v3+json')
-		.set('Authorization', `token ${token}`)
-		.then((response)=>{
-			return response.body.files[gistName].content;
-		})
-		.then((content)=>{
-			return request.patch(`https://api.github.com/gists/${crimeGistID}`)
-				.set('Accept', 'application/vnd.github.v3+json')
-				.set('Authorization', `token ${token}`)
-				.send({
-					description : '#20crimeteen',
-					files: {
-						[gistName] : {
-							content : `${content}\n\n${newContent}`
-						}
-					}
-				})
-		})
-		.catch((err)=>{
-			console.log(err);
-			Slack.error(err);
-		})
-};
+let lastMessage = {};
+let lastSender  = {};
 
 Slack.onMessage((msg)=>{
 	if(Slack.msgHas(msg.text, ['#20crimeteen', '#twentycrimeteen', '#20crimeteam', '#twentycrimeteam'])){
-		updateCrimeGist(`[${(new Date()).toLocaleDateString()}] ${msg.user}: ${msg.text}`)
-			.then(()=>{
-				Slack.sendAs('crimebot', 'police_car', msg.channel, `<https://gist.github.com/stolksdorf/24d22dfeecc3d02e419cad8739e8b672|#20crimeteen!>`)
-			});
+		Slack.sendAs('crimebot', 'police_car', msg.channel, `<https://gist.github.com/stolksdorf/${crimeGistID}|#20crimeteen!>`)
+		Gist.append(crimeGistID, {
+			'20crimeteen' : `\n\n\`[in #${msg.channel} on ${(new Date()).toLocaleDateString()} at ${(new Date()).toLocaleTimeString()}]\`<br />`
+				+ `**${lastSender[msg.channel]}**: ${lastMessage[msg.channel]}<br />`
+				+ `**${msg.user}**: ${msg.text}\n\n`
+		})
+	}else{
+		lastMessage[msg.channel] = msg.text;
+		lastSender[msg.channel] = msg.user;
 	}
 });
