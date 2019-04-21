eabf41d484d47a50d96bab309a0171e627f9c161
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2019-04-21 10:23:49 -0400

    Added process counter to migration process

--- bots/markovbot/new stuff/migration.js
+++ bots/markovbot/new stuff/migration.js
@@ -3,6 +3,8 @@ const Engine = require('./engine.js');
 const Storage = require('./storage.js');
 const S3 = require('./s3.js');
 
+const reduce = (obj,fn,init)=>Object.keys(obj).reduce((a,key)=>fn(a,obj[key],key),init);
+
 
 const sequence = async (arr, func)=>{
 	return arr.reduce((prom, val, key)=>{
@@ -17,8 +19,14 @@ module.exports = async (users)=>{
 		const data = await redis.get(user);
 		Storage.cacheStats(user, data.msgs, data.letters);
 
-		const newMapping = Engine.extendMapping('', data.weights);
-		console.log('created new map', Object.keys(data.weights).length);
+		const size = Object.keys(data.weights).length
+		let idx = 0;
+
+		const newMapping = reduce(data.weights, (acc, weights, seq)=>{
+			console.log(Math.round(idx++/size*100*100)/100);
+			return acc + Engine.utils.encodeFragment(seq, weights) + '\n'
+		},'');
+
 		await S3.upload(`${user}.map`, newMapping);
 		console.log('Uploaded to S3');
 	})
