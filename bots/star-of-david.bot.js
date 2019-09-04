4e286d52f896731a630f4bfd39f32ba5deacc7b0
Kellen Steffen <kellenwsteffen@gmail.com>
2019-09-04 11:14:53 -0400

    Add `:star_of_david:` check.

--- bots/star-of-david.bot.js
+++ bots/star-of-david.bot.js
@@ -2,7 +2,7 @@ const Slack = require('pico-slack');
 
 Slack.onReact((event) => {
 	const isProbablyADave = event.reaction.indexOf('dav') >= 0;
-	if(isProbablyADave) {
+	if(isProbablyADave && event.reaction !== 'star_of_david') {
 		Slack.react(event.item, 'star_of_david');
 	}
 });
