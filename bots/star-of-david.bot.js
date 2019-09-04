025d4369d6761e4653c17739bc9f719db500c82f
Kellen Steffen <kellenwsteffen@gmail.com>
2019-09-04 11:21:36 -0400

    Deconstruct event param.

--- bots/star-of-david.bot.js
+++ bots/star-of-david.bot.js
@@ -1,8 +1,8 @@
 const Slack = require('pico-slack');
 
-Slack.onReact((event) => {
-	const isProbablyADave = event.reaction.indexOf('dav') >= 0;
-	if(isProbablyADave && event.reaction !== 'star_of_david') {
-		Slack.react(event.item, 'star_of_david');
+Slack.onReact(({ reaction, item }) => {
+	const isProbablyADave = reaction.indexOf('dav') >= 0;
+	if(isProbablyADave && reaction !== 'star_of_david') {
+		Slack.react(item, 'star_of_david');
 	}
 });
