54dc6f667526996fd53cb9328af98c5b5fd291d5
Kellen Steffen <kellenwsteffen@gmail.com>
2019-09-09 10:36:45 -0400

    Catch all the things.

--- bots/star-of-david.bot.js
+++ bots/star-of-david.bot.js
@@ -2,7 +2,7 @@ const Slack = require('pico-slack');
 
 Slack.onReact(({ reaction, item }) => {
 	const isProbablyADave = reaction.indexOf('dav') >= 0;
-	if(isProbablyADave && reaction !== 'star_of_david') {
-		Slack.react(item, 'star_of_david');
+	if(isProbablyADave) {
+		Slack.react(item, 'star_of_david').catch(()=>null);
 	}
 });
