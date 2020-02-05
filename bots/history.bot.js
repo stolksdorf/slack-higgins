df757073463517aab13480f02abe88222a1c6194
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-02-04 22:39:56 -0500

    updating history to start using json instead of txt

--- bots/history.bot.js
+++ bots/history.bot.js
@@ -8,22 +8,23 @@ const S3 = require('../utils/s3.js');
 const MIN = 60 * 1000;
 const BucketName = config.get('historybot.bucket_name');
 const wait = async (n,val)=>new Promise((r)=>setTimeout(()=>r(val), n));
-const DIV = '\n\n'
 
 let HistoryStorage = {};
 
-
 const fetchHistory = async (channel)=>{
-	let channelData = await S3.fetch(BucketName, `${channel}.txt`);
-	if(HistoryStorage[channel]) channelData += DIV + HistoryStorage[channel];
-	return channelData;
+	let channelData;
+	try{
+		channelData = JSON.parse(await S3.fetch(BucketName, `${channel}.json`));
+	}catch(err){ channelData = [] }
+	if(!HistoryStorage[channel]) return channelData
+	return channelData.concat(HistoryStorage[channel]);
 }
 
 const saveChannel = async (channel)=>{
 	console.log(`Saving ${channel}...`);
 
 	const channelData = await fetchHistory(channel);
-	await S3.upload(BucketName, `${channel}.txt`, channelData);
+	await S3.upload(BucketName, `${channel}.json`, JSON.stringify(channelData));
 	delete HistoryStorage[channel];
 
 	console.log(`Finished ${channel}`);
@@ -46,38 +47,46 @@ const backupAll = async ()=>{
 };
 
 
-const parseMessage = (msg)=>{
-	return `[${msg.user}] {${datefns.format(new Date(), 'dd/MMM/yyyy - H:mm:ss')}} = ${msg.text}`
+const parseMessage = (msgObj)=>{
+	let res = {
+		id : msgObj.ts,
+		user : msgObj.user,
+		//date : datefns.format(new Date(), 'dd/MMM/yyy - H:mm:ss'), //for v2 of datefns
+		date : datefns.format(new Date(), 'DD/MMM/YYYY - H:mm:ss'),
+		msg : msgObj.text
+	}
+	if(msgObj.thread_ts) res.thread = msgObj.thread_ts;
+	return res;
 }
 
 const storeMessage = (msg)=>{
-	if(!msg.text) return;
-	HistoryStorage[msg.channel] = (HistoryStorage[msg.channel]
-			? HistoryStorage[msg.channel] + DIV
-			: '')
-		+ parseMessage(msg);
+	HistoryStorage[msg.channel] = (HistoryStorage[msg.channel] || []).concat(parseMessage(msg))
 };
 
-const uploadHistory = async (channel)=>{
+const uploadHistoryToSlack = async (channel)=>{
 	const content = await fetchHistory(channel);
 	const filename = `coolsville-${channel}-history.txt`
+
+	const file = content.map((entry)=>`[${entry.user} ${entry.date}]:${entry.msg}`).join('\n');
 
 	await request.post('https://slack.com/api/files.upload')
 		.field('token',  Slack.token)
 		.field('channels', channel)
 		.field('filename',  filename)
 		.field('filetype',  'txt')
-		.field('title', `${channel} history`)
-		.attach('file', Buffer.from(content), {filename})
+		.field('title', filename)
+		.attach('file', Buffer.from(file), {filename})
 }
 
 
-setInterval(backupAll, 20 * MIN)
+setInterval(backupAll, config.get('historybot:backup_rate'))
 
 Slack.onMessage(async (msg)=>{
-	if(Slack.msgHas(msg.text, 'higgins', 'history')){
-		return await uploadHistory(msg.channel);
+	if(Slack.msgHas(msg.text, 'higgins', 'history', ['please', 'plz'])){
+		return await uploadHistoryToSlack(msg.channel);
 	}
 
-	if(msg.text) storeMessage(msg);
+	if(msg.text && !config.get('historybot:ignored_channels').includes(msg.channel)){
+		storeMessage(msg);
+	}
 });
