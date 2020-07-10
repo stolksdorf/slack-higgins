15eb29fa102d86585831885b30a00c99fb20be0b
Jared Tyler <jared@jaredtyler.ca>
2020-07-09 22:11:31 -0400

    Tag users in reminder messages so the chosen one gets pinged.

--- bots/happinessncheer.bot.js
+++ bots/happinessncheer.bot.js
@@ -4,31 +4,31 @@ const config = require('../config');
 const {differenceInCalendarDays} = require('date-fns');
 
 const peeps = [
-	`Rbb`,
-	`Christian`,
-	`Evelyn`,
-	`LP`,
-	`Scott`,
-	`Rachel`,
-	`Tina`,
-	`David`,
-	`Katie`,
-	`Meg`,
-	`Greg`,
-	`Kellen`,
-	`Cathleen`,
-	`Chris`,
-	`Simon`,
-	`Carly`,
-	`Jared`,
-	`Mark`,
-	`Jenny`,
-	`Conrad`,
-	`Ross`,
-	`Ryan`,
-	'Christie',
-	'Sarah',
-	'Katie B',
+	`rebaybay`,
+	`christian`,
+	`evelyn`,
+	`lp`,
+	`scott`,
+	`rhenderson1993`,
+	`tskoops`,
+	`david`,
+	`katie`,
+	`meggeroni`,
+	`gleaver`,
+	`kellen`,
+	`cathleen`,
+	`chris`,
+	`simon`,
+	`carlygrayy`,
+	`jared`,
+	`mark`,
+	`jenny`,
+	`conradtwchow`,
+	`ross`,
+	`ryan`,
+	`christiefelker993`,
+	`sarahellen.w`,
+	`kclairebrown`,
 ];
 
 const PeepOffset = Number(config.get('happinessandcheerbot:peep_offset', true)); //So whatever date it is we land on the right person
@@ -39,16 +39,24 @@ const getSuggester = (offset=0) =>{
 	const delta = differenceInCalendarDays(now, yearStart);
 	const targetIdx = (delta + offset) % peeps.length
 	return peeps[(targetIdx + PeepOffset) % peeps.length];
-}
+};
+
+const mention = (user)=>{
+	const userId = Object.entries(Slack.users).reduce((acc, [id, name])=>{
+		return (name===user) ? id : acc;
+	}, null);
+	if (userId===null) return user;
+	return `<@${userId}>`;
+};
 
 cron.scheduleJob(`0 22 * * *`, ()=>{
 	const nextUp = getSuggester(1);
-	Slack.send('happiness-and-cheer', `Reminder: ${nextUp} will be picking theme for tomorrow.`);
+	Slack.send('happiness-and-cheer', `Reminder: ${mention(nextUp)} will be picking theme for tomorrow.`);
 });
 
 cron.scheduleJob(`0 9 * * *`, ()=>{
 	const theChoosenOne = getSuggester(0);
-	Slack.send('happiness-and-cheer', `Reminder: ${theChoosenOne} which theme will you bless us with today?`);
+	Slack.send('happiness-and-cheer', `Reminder: ${mention(theChoosenOne)} which theme will you bless us with today?`);
 });
 
 //console.log(getSuggester(0))
