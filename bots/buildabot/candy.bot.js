f50a48d07df8d0faf32aea51d0de0054f84cf485
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-08-22 12:26:31 -0400

    Adding memegen bot

---
+++ bots/buildabot/candy.bot.js
@@ -0,0 +1,29 @@
+
+
+// //https://duckduckgo.com/i.js?query=candy&o=json
+
+
+
+// //RelatedTopics > Icon
+
+
+
+
+
+// const pluck = (arr)=>arr[Math.floor(Math.random()*arr.length)];
+
+
+// const Slack = require('pico-slack');
+
+// const request = require('superagent');
+// const config = require('pico-conf');
+
+
+// request.get('https://duckduckgo.com/i.js?q=candy&atb=v205-1&iax=images&ia=images&o=json')
+// 	.then((res)=>{
+// 		console.log(res)
+// 		//return body.RelatedTopics
+// 	})
+// 	.then((rt)=>{
+// 		console.log(rt)
+// 	})
---
+++ bots/buildabot/memegen.bot.js
@@ -0,0 +1,55 @@
+const Slack = require('pico-slack');
+
+const request = require('superagent');
+const config = require('pico-conf');
+
+
+const memeTemplates = require('./popular_meme_templates.js');
+
+const pluck = (arr)=>arr[Math.floor(Math.random()*arr.length)];
+
+
+const getLastTwoMsgs = async (channelId)=>{
+	return Slack.api('conversations.history', {
+		channel : channelId,
+		limit : 3
+	}).then((res)=>{
+		return res.messages.map((x=>x.text)).slice(1).reverse()
+	})
+}
+
+const makeMeme = async (memeId, text0, text1)=>{
+	return request.post('https://api.imgflip.com/caption_image')
+		.set('Accept', 'application/json')
+		.query({
+			template_id : memeId,
+			username : 'stolksdorf',
+			password : config.get('memegen:imgflip_pwd'),
+			text0, text1
+		})
+		.then(({body})=>body.data.url)
+};
+
+const getMemeId = (text)=>{
+	return pluck(Object.keys(memeTemplates));
+}
+
+
+
+Slack.onMessage((evt)=>{
+	if(Slack.has(evt, 'memeit')){
+		getLastTwoMsgs(evt.channel_id)
+			.then(([msg1, msg2])=>{
+				return makeMeme(getMemeId(), msg1, msg2)
+			})
+			.then((memeUrl)=>{
+				Slack.alias('memebot', 'robot_face').send(evt.channel, memeUrl)
+			})
+			.catch((err)=>{
+				Slack.send(evt.channel, 'Oops, something went wrong')
+			})
+	}
+});
+
+
+
