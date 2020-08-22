ebd977f436709f27f11e44bd84854d13c97b75b7
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-08-22 16:28:41 -0400

    todo should be working

--- bots/scott_todo.bot.js
+++ bots/scott_todo.bot.js
@@ -2,24 +2,21 @@ const Slack = require('pico-slack');
 const config = require('pico-conf');
 const S3 = require('../utils/s3.js');
 
-const getTodayDateCode = ()=>require('date-fns').format(new Date(), 'DD-MM-YYYY');
+//const getTodayDateCode = ()=>require('date-fns').format(new Date(), 'DD-MM-YYYY');
 
 const isScott = (msg)=>msg.user == 'scoot' || msg.user == 'scott';
 
 const getTodo = async ()=>{
 
-	console.log(config.get('scott_todo_bot:bucket'),
-		`${getTodayDateCode()}.md`)
 
 	return await S3.fetch(
 		config.get('scott_todo_bot:bucket'),
-		`${getTodayDateCode()}.md`
+		`today.md`
 	);
 };
 
 const addItemTodo = async (item)=>{
 	const todo = await getTodo();
-
 
 	await S3.upload(
 		config.get('scott_todo_bot:bucket'),
@@ -29,7 +26,7 @@ const addItemTodo = async (item)=>{
 
 	return await S3.upload(
 		config.get('scott_todo_bot:bucket'),
-		`${getTodayDateCode()}.md`,
+		`today.md`,
 		todo + `\n- ${item}`
 	);
 };
