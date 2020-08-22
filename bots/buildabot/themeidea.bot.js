59fe3cf25c4beb430518f1aa6f772dcd95ac835a
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-08-22 11:36:29 -0400

    Adding theme suggester bot

---
+++ bots/buildabot/themeidea.bot.js
@@ -0,0 +1,29 @@
+const Slack = require('pico-slack');
+
+const prevThemes = require('./prev_themes.js');
+
+
+const pluck = (arr)=>arr[Math.floor(Math.random()*arr.length)];
+
+Slack.onMessage((msg)=>{
+	if(msg.channel == 'happiness-and-cheer'
+		&& Slack.has(msg, [`can't think`, 'not sure', 'suggest', 'suggestion'], ['theme'])){
+		return Slack.alias('themebot', 'lightning').send(msg.channel,
+			pluck([
+				'What about',
+				'How about',
+				'Play that theme again Jack',
+				'This one was fun',
+				'Round two, FIGHT',
+				'Have you considered',
+				'Why not this one again?',
+				'I gotchu',
+				'This is theme you are looking for',
+				'np babe'
+			])
+			+ ': *' +
+			pluck(prevThemes)
+			+'*'
+		);
+	}
+});
