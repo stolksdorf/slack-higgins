5681e6c4b0ea1740e12ae0dfc67894e9ccbfd60d
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-10-17 10:25:09 -0400

    Updating the twitter thread bot to be a bit more interactive

--- bots/twitter_thread.bot.js
+++ bots/twitter_thread.bot.js
@@ -1,12 +1,22 @@
 const Slack = require('pico-slack');
-
 const twitterRegex = (str)=>(/https:\/\/(mobile\.)?twitter\.com\/[a-z_0-9]+\/status\/(\d+)/.exec(str) || [])[2];
+
+let Cache = {};
 
 Slack.onMessage((msg)=>{
 	const twitterId = twitterRegex(msg.text);
 	if(twitterId){
+		Cache[msg.ts] = twitterId;
+		Slack.react(msg, ':scroll:');
+	}
+});
+
+Slack.onReact((evt)=>{
+	if(evt.reaction == 'scroll' && Cache[evt.item.ts]){
 		Slack
 			.alias('twitter_thread_bot', ':scroll:')
-			.send(msg.channel, `https://threadreaderapp.com/thread/${twitterId}.html`);
+			.send(evt.channel, `https://threadreaderapp.com/thread/${Cache[evt.item.ts]}.html`);
+
+		delete Cache[evt.item.ts];
 	}
-})
+});
