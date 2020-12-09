8c565fa8692eab2ab17dd99c320503d9fcfba190
Kellen Steffen <kellenwsteffen@gmail.com>
2020-12-09 00:09:55 -0500

    `Slack.emitter.on` -> `Slack.onEvent`

--- bots/departurebot/departure.bot.js
+++ bots/departurebot/departure.bot.js
@@ -4,6 +4,6 @@ const Slack = require('pico-slack');
 
 const quotes = fs.readFileSync('./bots/departurebot/departure.quotes.txt', 'utf8').split('\n');
 
-Slack.emitter.on('member_left_channel', (data)=>{
+Slack.onEvent('member_left_channel', (data)=>{
 	Slack.msg(data, `_${_.sample(quotes)}_`);
 });
