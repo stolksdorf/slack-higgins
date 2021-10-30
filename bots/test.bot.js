45185b4804ae24fc609670da071a5877b37377c4
scott-tolksdorf <scott.tolksdorf@applyboard.com>
2021-10-30 13:38:36 -0400

    pico-slack bump

--- bots/test.bot.js
+++ bots/test.bot.js
@@ -5,7 +5,10 @@ Slack.onMessage((msg)=>{
 	if(msg.isDirect && msg.text == 'info'){
 		Slack.log(msg)
 		const {users, channels, dms, bots} = Slack;
-		Slack.send(msg.user, "```"+JSON.stringify({users, channels, dms, bots}, null, '  ')+"```");
+		Slack.send(msg.user, "```"+JSON.stringify({users}, null, '  ')+"```");
+		Slack.send(msg.user, "```"+JSON.stringify({channels}, null, '  ')+"```");
+		Slack.send(msg.user, "```"+JSON.stringify({dms}, null, '  ')+"```");
+		Slack.send(msg.user, "```"+JSON.stringify({bots}, null, '  ')+"```");
 	}
 
 	if(msg.isDirect && msg.text == 'carly'){
