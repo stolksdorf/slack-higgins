6d502c0d1861961a5b8d2eb37b2cbcaa74a7c726
Meg Kirkland <meg.kirkl@gmail.com>
2022-01-05 10:18:47 -0500

    I'm done after this I promise

--- bots/crime.bot.js
+++ bots/crime.bot.js
@@ -8,10 +8,10 @@ let lastMessage = {};
 let lastSender  = {};
 
 Slack.onMessage((msg)=>{
-	if(Slack.has(msg.text, ['#bigcoupenergy','coup','#twentytwentycoup'])){
+	if(Slack.has(msg.text, ['#bigcoupenergy','#twentytwentycoup'])){
 		Slack.sendAs('coupbot', 'eggplant', msg.channel, `:man-with-bunny-ears-partying: <https://gist.github.com/stolksdorf/${crimeGistID}|#TwentyTwentyCoup!> :man-with-bunny-ears-partying:`)
 		Gist.append(crimeGistID, {
-			'BigDecadeEnergy' : `\n\n\`[in #${msg.channel} on ${(new Date()).toLocaleDateString()} at ${(new Date()).toLocaleTimeString()}]\`<br />`
+			'coupbot' : `\n\n\`[in #${msg.channel} on ${(new Date()).toLocaleDateString()} at ${(new Date()).toLocaleTimeString()}]\`<br />`
 				+ `**${lastSender[msg.channel]}**: ${lastMessage[msg.channel]}<br />`
 				+ `**${msg.user}**: ${msg.text}\n\n`
 		})
