4b76650a420e39f7c5d1209f7fa68281e7508ed6
stolksdorf <scott.tolksdorf@gmail.com>
2022-05-07 12:39:34 -0400

    Adding in apollo theater bot and updating pico-slack

---
+++ bots/star-of-david.bot.js
@@ -0,0 +1,8 @@
+const Slack = require('pico-slack');
+
+Slack.onReact(({ reaction, item }) => {
+	const isProbablyADave = reaction.indexOf('dav') >= 0;
+	if(isProbablyADave) {
+		Slack.react(item, 'star_of_david').catch(()=>null);
+	}
+});
